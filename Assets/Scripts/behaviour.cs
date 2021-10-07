6dfbbdf09ab7ec705334bab2750cfa6d201c4edb
Vultur Iulian <iulyusdiaz@yahoo.com>
2021-10-07 20:06:09 +0300

    Small adjustment

--- Assets/Scripts/behaviour.cs
+++ Assets/Scripts/behaviour.cs
@@ -16,20 +16,11 @@ public class behaviour : MonoBehaviour {
     void Update() {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-        // for(int i = 0; i < players.Length; i++) {
-        //     Debug.Log("x: " + i + " " + players[i].transform.position.x);
-        // }
-
-        players = players.Where(val => val.transform.position.x < 10).ToArray();
-
-        Debug.Log("unity test: " + players.Length);
         if(players.Length > 1) {
             Vector3 pos1 = players[0].transform.position;
             Vector3 pos2 = players[1].transform.position;
 
             float dist = Vector3.Distance(pos1, pos2);
-            //float dist = (float) Math.Sqrt((pos1.x - pos2.x) * (pos1.x - pos2.x) + (pos1.y - pos2.y) * (pos1.y - pos2.y) + (pos1.z - pos2.z) * (pos1.z - pos2.z));
-            Debug.Log("test: " + (dist < distance) + " " + dist);
 
             Animator animator1 = players[0].GetComponent<Animator>();
             Animator animator2 = players[1].GetComponent<Animator>();
